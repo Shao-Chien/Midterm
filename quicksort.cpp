75812b96fae12a6f5b3b5c1d16764c6ea58d188f
Shao-Chien <magicdebater@gmail.com>
2015-04-29 02:37:36 +0800

    Complete !

--- quicksort.cpp
+++ quicksort.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 int partition(int v[],int l,int u);
 void quicksort(int v[],int a,int b);
+void quickmedian(int A[],int a,int b);
 
 int main()
 {
@@ -12,12 +13,14 @@ int main()
 	srand(time(&T));
 	N = 10;
 	v = (int*) malloc (N* sizeof(int)); // 開n個空間 
+	printf("Before sorting : ");
 	for(i=1;i<N;++i) // 從 N=1 開始 
 	{
 		v[i] = rand()%100;
 		printf("%d,",v[i]);
 	}
 	printf("\n");
+	printf("After sorting :  ");
 		
     quicksort(v,0,N-1);
     for(i=0;i<N-1;++i) 
@@ -25,6 +28,8 @@ int main()
 		printf("%d,",v[i]);
 	}
 	printf("\n");
+	
+	quickmedian(v,0,N-1);
 		
 }
 
@@ -37,6 +42,24 @@ void quicksort(int A[],int a,int b)
                 quicksort(A,a,q-1);
                 quicksort(A,q+1,b);
         }
+}
+
+void quickmedian(int A[],int a,int b)
+{
+	if(a<b)
+	{
+		int q;
+		q = partition(A,a,b);
+		if(q=(b-a-1)/2.)
+		{
+			printf("The median is %d .",A[q]);
+		}
+		else
+		{
+		quickmedian(A,a,q-1);
+		quickmedian(A,q+1,b);
+		}
+	}
 }
 
 int partition(int A[],int l,int u)
